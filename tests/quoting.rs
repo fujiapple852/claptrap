0e628b15dee2b6949cf9331ab8562c1ac657b25f
FujiApple <fujiapple852@gmail.com>
2025-06-23 21:01:32 +0800

    WIP3

--- tests/quoting.rs
+++
@@ -1,8 +0,0 @@
-mod util;
-use test_case::test_case;
-
-#[test_case(case!("quoting", "single_value_with_quote"))]
-#[test_case(case!("quoting", "many_values_special_chars"))]
-fn test_quoting((name, spec, args): (&str, &str, &str)) {
-    insta::assert_snapshot!(name, util::run(spec, args));
-}
