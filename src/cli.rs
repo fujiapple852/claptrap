1a72b59da286f91a1887ea78cc740f926ec34f4f
FujiApple <fujiapple852@gmail.com>
2025-07-01 23:20:28 +0800

    feat: improve tool cli subcommand handling

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,26 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use std::ffi::OsString;
 use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[command(version, about, long_about = None, arg_required_else_help(true))]
+#[command(
+    version,
+    about,
+    arg_required_else_help(true),
+    args_conflicts_with_subcommands(true),
+    subcommand_negates_reqs(true)
+)]
 pub struct Cli {
     /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE", env = "CLAPTRAP_SPEC")]
-    pub spec: PathBuf,
+    #[arg(
+        short,
+        long,
+        required(true),
+        value_name = "FILE",
+        env = "CLAPTRAP_SPEC"
+    )]
+    pub spec: Option<PathBuf>,
 
     /// The format of the spec file
     #[arg(long, value_name = "FORMAT", env = "CLAPTRAP_SPEC_FORMAT", default_value_t = SpecFormat::Auto)]
@@ -30,6 +42,9 @@ pub struct Cli {
 pub enum SubCommand {
     /// Generate shell completion
     Completion {
+        #[command(flatten)]
+        spec: SpecInfo,
+
         /// The shell to generate completions for
         #[arg(value_enum)]
         shell: clap_complete::Shell,
@@ -40,13 +55,19 @@ pub enum SubCommand {
     },
     /// Generate ROFF man page
     Man {
+        #[command(flatten)]
+        spec: SpecInfo,
+
         /// The output file for the ROFF man page
         #[arg(short, long, value_name = "FILE")]
         output: Option<PathBuf>,
     },
     /// Generate a template script
     Script {
-        /// The shell to generate completions for
+        #[command(flatten)]
+        spec: SpecInfo,
+
+        /// The shell to generate template script for
         #[arg(value_enum)]
         shell: Shell,
 
@@ -56,6 +77,9 @@ pub enum SubCommand {
     },
     /// Generate documentation
     Doc {
+        #[command(flatten)]
+        spec: SpecInfo,
+
         /// The format of the documentation
         #[arg(value_enum, short = 'f', long, default_value_t = DocFormat::Markdown)]
         format: DocFormat,
@@ -64,6 +88,17 @@ pub enum SubCommand {
         #[arg(short, long, value_name = "FILE")]
         output: Option<PathBuf>,
     },
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct SpecInfo {
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE", env = "CLAPTRAP_SPEC")]
+    pub spec: PathBuf,
+
+    /// The format of the spec file
+    #[arg(long, value_name = "FORMAT", env = "CLAPTRAP_SPEC_FORMAT", default_value_t = SpecFormat::Auto)]
+    pub spec_format: SpecFormat,
 }
 
 /// Shell with template script available.
@@ -75,7 +110,7 @@ pub enum Shell {
     /// Friendly Interactive `SHell` (fish)
     Fish,
     /// `PowerShell`
-    #[allow(clippy::enum_variant_names)]
+    #[expect(clippy::enum_variant_names)]
     PowerShell,
     /// Z `SHell` (zsh)
     Zsh,
