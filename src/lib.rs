a0861a888348daeb04df1ce969ad17d4c351e461
FujiApple <fujiapple852@gmail.com>
2025-07-08 23:28:28 +0800

    refactor: create `clap_ext.rs` and move `IsManyEx`

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,6 @@
 #![forbid(unsafe_code)]
 
+mod clap_ext;
 pub mod output;
 pub mod parse;
 pub mod types;
-
-/// Extension trait for `clap::Arg` to determine if it is many-valued.
-trait IsManyEx {
-    /// Returns true if the argument is many-valued.
-    fn is_many(&self) -> bool;
-}
-
-impl IsManyEx for clap::Arg {
-    fn is_many(&self) -> bool {
-        self.get_num_args()
-            .map(|r| r.max_values() > 1)
-            .or_else(|| self.get_value_delimiter().map(|_| true))
-            .unwrap_or(false)
-    }
-}
