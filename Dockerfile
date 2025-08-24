9e8963e103cce8d6716fd0803f8af9d0fe599e88
FujiApple <fujiapple852@gmail.com>
2025-08-24 13:33:33 +0800

    refactor: introduce workspace

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,27 @@ RUN rustup target add x86_64-unknown-linux-musl
 WORKDIR /app
 COPY Cargo.toml /app
 COPY Cargo.lock /app
-RUN mkdir -p src
-COPY Cargo.toml /app/Cargo.toml
-COPY templates /app/templates
+RUN mkdir -p /app/crates/claptrap/src
+RUN mkdir -p /app/crates/claptrap-core/src
+RUN mkdir -p /app/crates/gen-api-doc/src
+COPY crates/claptrap/Cargo.toml /app/crates/claptrap/Cargo.toml
+COPY crates/claptrap-core/Cargo.toml /app/crates/claptrap-core/Cargo.toml
+COPY crates/gen-api-doc/Cargo.toml /app/crates/gen-api-doc/Cargo.toml
+
+COPY crates/claptrap/templates /app/crates/claptrap/templates
 
 # dummy build to cache dependencies
-RUN echo "fn main() {}" > /app/src/main.rs
+RUN echo "fn main() {}" > /app/crates/claptrap/src/main.rs
+RUN touch /app/crates/claptrap-core/src/lib.rs
+RUN touch /app/crates/gen-api-doc/src/lib.rs
 RUN cargo build --release --target=x86_64-unknown-linux-musl --package claptrap
 
 # copy the actual application code and build
-COPY src /app/src
+COPY crates/claptrap/src /app/crates/claptrap/src
+COPY crates/claptrap-core/src /app/crates/claptrap-core/src
 COPY README.md /app
-RUN cargo clean --release --target=x86_64-unknown-linux-musl -p claptrap
+COPY README.md /app/crates/claptrap
+RUN cargo clean --release --target=x86_64-unknown-linux-musl -p claptrap-core
 RUN cargo build --release --target=x86_64-unknown-linux-musl
 
 FROM alpine
---
+++ crates/claptrap/src/main.rs
@@ -0,0 +1,283 @@
+use anstream::ColorChoice;
+use anyhow::anyhow;
+use clap::builder::StyledStr;
+use clap::Parser;
+use claptrap_core::template_values;
+use claptrap_core::Command;
+use claptrap_core::{CatCmd, ExitCode, Output};
+use cli::{DocFormat, OutputFormat, Shell, SpecFormat, SpecInfo, SubCommand};
+use minijinja::{context, Environment};
+use std::ffi::OsString;
+use std::io::Write;
+use std::iter::once;
+use std::panic;
+use std::panic::AssertUnwindSafe;
+use std::path::{Path, PathBuf};
+use std::process::exit;
+
+mod cli;
+mod error;
+
+fn main() -> anyhow::Result<()> {
+    let cli = cli::Cli::parse();
+    match cli.command {
+        Some(SubCommand::Completion {
+            spec: SpecInfo { spec, spec_format },
+            shell,
+            output,
+        }) => {
+            run_generate_completions(&spec, spec_format, shell, output)?;
+            exit(0);
+        }
+        Some(SubCommand::Man {
+            spec: SpecInfo { spec, spec_format },
+            output,
+        }) => {
+            run_generate_man(&spec, spec_format, output)?;
+            exit(0);
+        }
+        Some(SubCommand::Script {
+            spec: SpecInfo { spec, spec_format },
+            shell,
+            output,
+        }) => {
+            run_generate_template(&spec, spec_format, &shell, output)?;
+            exit(0);
+        }
+        Some(SubCommand::Doc {
+            spec: SpecInfo { spec, spec_format },
+            format,
+            output,
+        }) => {
+            run_generate_doc(&spec, spec_format, format, output)?;
+            exit(0);
+        }
+        Some(SubCommand::Schema { output }) => {
+            run_generate_schema(output)?;
+            exit(0);
+        }
+        None => {
+            // As we are being called from an 'eval' in a shell, we have to be
+            // careful that everything we output is "eval safe". This includes
+            // all errors from the tool and even panics.
+            //
+            // We use `catch_unwind` to catch panics and return them in a way that
+            // is eval safe. If the user has set `--show-panic`, we will not suppress
+            // the panic messages, which can be useful for debugging.
+            if !cli.show_panic {
+                panic::set_hook(Box::new(|_| {}));
+            }
+            let mut stdout =
+                anstream::AutoStream::new(std::io::stdout().lock(), ColorChoice::AlwaysAnsi);
+            match panic::catch_unwind(AssertUnwindSafe(|| {
+                let spec = cli
+                    .spec
+                    .ok_or_else(|| anyhow!("internal error: spec was None"))?;
+                run_app(&spec, cli.spec_format, cli.args)
+            })) {
+                Ok(val) => match val {
+                    Ok(output) => {
+                        write!(stdout, "{}", output.render(cli.output_format.into()))?;
+                        stdout.flush()?;
+                        exit(0);
+                    }
+                    Err(err) => {
+                        write!(stdout, "{}", err.render(cli.output_format))?;
+                        stdout.flush()?;
+                        exit(0);
+                    }
+                },
+                Err(err) => {
+                    let panic = panic_output(&err);
+                    write!(stdout, "{}", panic.render(cli.output_format.into()))?;
+                    stdout.flush()?;
+                    exit(0);
+                }
+            }
+        }
+    }
+}
+
+fn run_generate_completions(
+    spec_path: &Path,
+    spec_format: SpecFormat,
+    shell: clap_complete::Shell,
+    output: Option<PathBuf>,
+) -> anyhow::Result<()> {
+    let spec = parse_spec(spec_path, spec_format)?;
+    let mut clap_cmd = clap::Command::from(spec);
+    let name = clap_cmd.get_name().to_string();
+    let mut buffer: Vec<u8> = vec![];
+    clap_complete::generate(shell, &mut clap_cmd, name, &mut buffer);
+    if let Some(output_path) = output {
+        std::fs::write(output_path, buffer)?;
+    } else {
+        std::io::stdout().write_all(&buffer)?;
+    }
+    Ok(())
+}
+
+fn run_generate_man(
+    spec_path: &Path,
+    spec_format: SpecFormat,
+    output: Option<PathBuf>,
+) -> anyhow::Result<()> {
+    let spec = parse_spec(spec_path, spec_format)?;
+    let clap_cmd = clap::Command::from(spec);
+    let mut buffer: Vec<u8> = vec![];
+    clap_mangen::Man::new(clap_cmd).render(&mut buffer)?;
+    if let Some(output_path) = output {
+        std::fs::write(output_path, buffer)?;
+    } else {
+        std::io::stdout().write_all(&buffer)?;
+    }
+    Ok(())
+}
+
+fn run_generate_template(
+    spec_path: &Path,
+    spec_format: SpecFormat,
+    shell: &Shell,
+    output: Option<PathBuf>,
+) -> anyhow::Result<()> {
+    let spec = parse_spec(spec_path, spec_format)?;
+    let clap_cmd = clap::Command::from(spec);
+    let template = match shell {
+        Shell::Bash => include_str!("../templates/bash_template.sh.j2"),
+        Shell::Fish => include_str!("../templates/fish_template.fish.j2"),
+        Shell::Zsh => include_str!("../templates/zsh_template.sh.j2"),
+        Shell::PowerShell => include_str!("../templates/powershell_template.ps1.j2"),
+    };
+    let values = template_values(&clap_cmd);
+    let mut env = Environment::new();
+    env.set_trim_blocks(true);
+    env.set_lstrip_blocks(true);
+    env.add_template("script", template)?;
+    let template = env.get_template("script")?;
+    let rendered = template.render(context! { spec => spec_path, values => values })?;
+    if let Some(output_path) = output {
+        std::fs::write(output_path, rendered)?;
+    } else {
+        std::io::stdout().write_all(rendered.as_bytes())?;
+    }
+    Ok(())
+}
+
+fn run_generate_doc(
+    spec_path: &Path,
+    spec_format: SpecFormat,
+    doc_format: DocFormat,
+    output: Option<PathBuf>,
+) -> anyhow::Result<()> {
+    let spec = parse_spec(spec_path, spec_format)?;
+    let clap_cmd = clap::Command::from(spec);
+    let markdown = clap_markdown::help_markdown_command(&clap_cmd);
+    let bytes = match doc_format {
+        DocFormat::Markdown => markdown.into_bytes(),
+    };
+    if let Some(output_path) = output {
+        std::fs::write(output_path, bytes)?;
+    } else {
+        std::io::stdout().write_all(&bytes)?;
+    }
+    Ok(())
+}
+
+fn run_generate_schema(output: Option<PathBuf>) -> anyhow::Result<()> {
+    let schema = schemars::schema_for!(Command);
+    let bytes = serde_json::to_vec_pretty(&schema)?;
+    if let Some(output_path) = output {
+        std::fs::write(output_path, bytes)?;
+    } else {
+        std::io::stdout().write_all(&bytes)?;
+    }
+    Ok(())
+}
+
+fn run_app(
+    spec_path: &Path,
+    spec_format: SpecFormat,
+    args: Vec<OsString>,
+) -> error::Result<Output> {
+    let spec = parse_spec(spec_path, spec_format)?;
+    let name = spec.get_name().to_string();
+    let args = once(OsString::from(name)).chain(args);
+    Ok(claptrap_core::parse(spec, args))
+}
+
+fn parse_spec(spec_path: &Path, spec_format: SpecFormat) -> anyhow::Result<Command> {
+    if spec_path == PathBuf::from("-") {
+        parse_spec_stdin(spec_format)
+    } else {
+        parse_spec_file(spec_path, spec_format)
+    }
+}
+
+fn parse_spec_stdin(spec_format: SpecFormat) -> anyhow::Result<Command> {
+    let spec = std::io::read_to_string(std::io::stdin())?;
+    match spec_format {
+        SpecFormat::Auto => {
+            if let Ok(cmd) = serde_json::from_str::<Command>(&spec) {
+                Ok(cmd)
+            } else if let Ok(cmd) = serde_yaml::from_str::<Command>(&spec) {
+                Ok(cmd)
+            } else if let Ok(cmd) = toml::from_str::<Command>(&spec) {
+                Ok(cmd)
+            } else {
+                Err(anyhow!("Failed to detect spec format, please specify a format explicitly with --spec-format (-f)"))?
+            }
+        }
+        SpecFormat::Json => Ok(serde_json::from_str::<Command>(&spec)?),
+        SpecFormat::Yaml => Ok(serde_yaml::from_str::<Command>(&spec)?),
+        SpecFormat::Toml => Ok(toml::from_str::<Command>(&spec)?),
+    }
+}
+
+fn parse_spec_file(spec_path: &Path, spec_format: SpecFormat) -> anyhow::Result<Command> {
+    let spec = std::fs::read_to_string(spec_path)?;
+    match spec_format {
+        SpecFormat::Auto => {
+            if let Some(ext) = spec_path.extension().and_then(|s| s.to_str()) {
+                match ext {
+                    "json" => Ok(serde_json::from_str::<Command>(&spec)?),
+                    "yaml" | "yml" => Ok(serde_yaml::from_str::<Command>(&spec)?),
+                    "toml" => Ok(toml::from_str::<Command>(&spec)?),
+                    _ => Err(anyhow::anyhow!("Unsupported spec format: {}", ext))?,
+                }
+            } else {
+                Err(anyhow::anyhow!(
+                    "No file extension found for spec format detection, please specify a format explicitly with --spec-format (-f)"
+                ))?
+            }
+        }
+        SpecFormat::Json => Ok(serde_json::from_str::<Command>(&spec)?),
+        SpecFormat::Yaml => Ok(serde_yaml::from_str::<Command>(&spec)?),
+        SpecFormat::Toml => Ok(toml::from_str::<Command>(&spec)?),
+    }
+}
+
+#[expect(clippy::coerce_container_to_any)]
+fn panic_output(err: &Box<dyn std::any::Any + Send>) -> Output {
+    let panic_message = if let Some(message) = err.downcast_ref::<String>() {
+        message
+    } else if let Some(message) = err.downcast_ref::<&str>() {
+        message
+    } else {
+        "An unexpected panic occurred"
+    };
+    Output::Cat(CatCmd::new(
+        StyledStr::from(format!("{panic_message}\n")),
+        ExitCode::Panic,
+        clap::ColorChoice::Auto,
+    ))
+}
+
+impl From<OutputFormat> for claptrap_core::OutputFormat {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Posix => Self::Posix,
+            OutputFormat::Fish => Self::Fish,
+            OutputFormat::PowerShell => Self::PowerShell,
+        }
+    }
+}
